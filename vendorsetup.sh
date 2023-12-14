d24ed7487cb19072a1df7a48db63a12033d047ec
Liwhy <levi.toth2005@gmail.com>
2023-12-14 21:07:06 +0100

    denniz: vendorsetup: Fix dependecy clone method

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,47 +11,31 @@ if ! history | tail -n 1 | grep -q "denniz"; then return; fi
 # Go to root of source
 cd "$ANDROID_BUILD_TOP"
 
-# Hack MTK RIL libs for USSD and incoming calls
-# see https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086
-BLOB_ROOT="$ANDROID_BUILD_TOP"/vendor/oneplus/denniz/proprietary
-
-for blob in $BLOB_ROOT/lib64/libmtk-ril.so; do
-    sed -i \
-        -e 's/AT+EAIC=2/AT+EAIC=3/g' \
-        $blob
-done
+# Clone dependencies
+POCKET=packages/apps/PocketMode
+if ! [ -d "$POCKET" ]; then git clone --depth=1 https://github.com/nishant6342/packages_apps_PocketMode.git packages/apps/PocketMode
+fi
+PARTS=packages/apps/OneplusParts
+if ! [ -d "$PARTS" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/packages_apps_OneplusParts.git packages/apps/OneplusParts
+fi
+KERNEL=kernel/oneplus/denniz
+if ! [ -d "$KERNEL" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/kernel_oneplus_denniz.git kernel/oneplus/denniz
+fi
+VENDOR=vendor/oneplus/denniz
+if ! [ -d "$VENDOR" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/vendor_oneplus_denniz.git vendor/oneplus/denniz
+fi
+IMS=vendor/oneplus/IMS-denniz
+if ! [ -d "$IMS" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/vendor_oneplus_IMS-denniz.git vendor/oneplus/IMS-denniz
+fi
+FIRMWARE=vendor/firmware/denniz
+if ! [ -d "$FIRMWARE" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/vendor_firmware_denniz.git vendor/firmware/denniz
+fi
+PROTON=prebuilts/clang/host/linux-x86/proton
+if ! [ -d "$PROTON" ]; then git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton
+fi
 
 # Apply denniz patches
 bash device/oneplus/denniz/patches/patch.sh
-
-# Clone dependencies
-if [ -a "$ANDROID_BUILD_TOP/packages/apps/PocketMode" ]; then
-    git clone https://github.com/nishant6342/packages_apps_PocketMode.git packages/apps/PocketMode
-fi
-
-if [ -a "$ANDROID_BUILD_TOP/packages/apps/OneplusParts" ]; then
-    git clone https://github.com/2by2-Project-Devices/packages_apps_OneplusParts.git packages/apps/OneplusParts
-fi
-
-if [ -a "$ANDROID_BUILD_TOP/kernel/oneplus/denniz" ]; then
-    git clone https://github.com/2by2-Project-Devices/kernel_oneplus_denniz.git kernel/oneplus/denniz
-fi
-
-if [ -a "$ANDROID_BUILD_TOP/vendor/oneplus/denniz" ]; then
-    git clone https://github.com/2by2-Project-Devices/vendor_oneplus_denniz.git vendor/oneplus/denniz
-fi
-
-if [ -a "$ANDROID_BUILD_TOP/vendor/oneplus/IMS-denniz" ]; then
-    git clone https://github.com/2by2-Project-Devices/vendor_oneplus_IMS-denniz.git vendor/oneplus/IMS-denniz
-fi
-
-if [ -a "$ANDROID_BUILD_TOP/vendor/firmware/denniz" ]; then
-    git clone https://github.com/2by2-Project-Devices/vendor_firmware_denniz.git vendor/firmware/denniz
-fi
-
-if [ -a "$ANDROID_BUILD_TOP/prebuilts/clang/host/linux-x86/proton" ]; then
-    git clone https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton
-fi
 
 # Return to saved directory
 cd $CUR_DIR ; unset CUR_DIR
