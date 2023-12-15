2a67284733b57613ba28387d6c163fd57bbe8537
Liwhy <levi.toth2005@gmail.com>
2023-12-15 19:36:17 +0100

    denniz: viper: Update directory layout

--- prebuilt/viper/viper.mk
+++ prebuilt/viper/viper.mk
@@ -1,5 +1,10 @@
-PRODUCT_SOONG_NAMESPACES += \
-    device/oneplus/denniz/prebuilt/viper
+BUILD_PATH := device/oneplus/denniz/prebuilt/viper
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(BUILD_PATH)/,vendor)
 
 PRODUCT_PACKAGES += \
-    Viper4Android
+    ViPER4AndroidFX
+
+# Time to relax
+RELAX_USES_LIBRARY_CHECK := true
