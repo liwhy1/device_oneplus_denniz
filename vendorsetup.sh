3135549d0f894a1c2c3dc604f5210fdf2624c97e
Liwhy <levi.toth2005@gmail.com>
2024-01-03 19:33:16 +0100

    denniz: vendorsetup: Adjust layout

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,9 +12,6 @@ if ! history | tail -n 1 | grep -q "denniz"; then return; fi
 cd "$ANDROID_BUILD_TOP"
 
 # Clone dependencies
-PARTS=packages/apps/OneplusParts
-if ! [ -d "$PARTS" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/packages_apps_OneplusParts.git packages/apps/OneplusParts
-fi
 KERNEL=kernel/oneplus/denniz
 if ! [ -d "$KERNEL" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/kernel_oneplus_denniz.git kernel/oneplus/denniz
 fi
@@ -29,6 +26,9 @@ if ! [ -d "$FIRMWARE" ]; then git clone --depth=1 https://github.com/2by2-Projec
 fi
 PROTON=prebuilts/clang/host/linux-x86/proton
 if ! [ -d "$PROTON" ]; then git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton
+fi
+PARTS=packages/apps/OneplusParts
+if ! [ -d "$PARTS" ]; then git clone --depth=1 https://github.com/2by2-Project-Devices/packages_apps_OneplusParts.git packages/apps/OneplusParts
 fi
 EXTRAS=packages/apps/OPlusExtras
 if ! [ -d "$EXTRAS" ]; then git clone --depth=1 https://github.com/nattolecats/packages_apps_OPlusExtras.git packages/apps/OPlusExtras
