f32689bb51f5c696edef45716623b5b7a234fd0d
Liwhy <levi.toth2005@gmail.com>
2024-02-15 14:45:02 +0100

    denniz: overlays: Update values
    Preparing target switch

--- device.mk
+++ device.mk
@@ -25,8 +25,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-12288-dalvik-heap.mk
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Call proprietary blob setup
-$(call inherit-product, vendor/oneplus/denniz/denniz-vendor.mk)
-$(call inherit-product, device/oplus/camera/camera.mk)
+#$(call inherit-product, vendor/oneplus/denniz/denniz-vendor.mk)
+#$(call inherit-product, device/oplus/camera/camera.mk)
 $(call inherit-product-if-exists, packages/apps/prebuilt-apps/prebuilt-apps.mk)
 $(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
 $(call inherit-product-if-exists, packages/apps/PocketMode/pocket_mode.mk)
@@ -288,7 +288,13 @@ PRODUCT_PACKAGES += \
     SystemUIOverlay \
     SettingsOverlay \
     TelephonyOverlay \
-    CarrierConfigOverlay
+    CarrierConfigOverlay \
+    TetheringConfigOverlay \
+    WifiOverlay \
+    DozeOverlaySystem \
+    DozeOverlaySystemUI \
+    OplusDozeOverlay \
+    DeviceSettingsOverlay
 
 # Enforce RRO targets
 PRODUCT_ENFORCE_RRO_TARGETS := *
@@ -297,7 +303,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/permissions/com.oplus.camera.unit.sdk_product.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.oplus.camera.unit.sdk_product.xml \
     $(DEVICE_PATH)/configs/permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_camera_default_grant_permissions_list.xml \
-    $(DEVICE_PATH)/configs/permissions/APU_SYS.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/APU_SYS.xml \
+  $(DEVICE_PATH)/configs/permissions/APU_SYS.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/APU_SYS.xml \
     $(DEVICE_PATH)/configs/sysconfig/hiddenapi-package-whitelist-oplus-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oplus-system.xml
 
 # ORMS
@@ -424,15 +430,6 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.mtk.rc
 
-# Rro
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay \
-    WifiOverlay \
-    DozeOverlaySystem \
-    DozeOverlaySystemUI \
-    OplusDozeOverlay \
-    DeviceSettingsOverlay
-
 # Soundtrigger
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl \
@@ -485,8 +482,8 @@ PRODUCT_BOOT_JARS += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common \
-    oplus-support-wrapper
-#    oplus-framework \
+    oplus-support-wrapper \
+    oplus-framework 
 
 # Thermal
 PRODUCT_PACKAGES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,61 +1,41 @@
-if [ -n "${CLEAN_DT_REPOS}" ]; then
-    if [ "$CLEAN_DT_REPOS" = "True" ]; then
-        echo "Cleaning old repos before cloning"
-        rm -rf vendor/oneplus
-        rm -rf kernel/oneplus
-        rm -rf packages/apps/prebuilt-apps
-        rm -rf device/mediatek/sepolicy_vndr
-        rm -rf device/oplus
-        rm -rf packages/apps/RealmeParts
-        rm -rf packages/apps/PocketMode
-        rm -rf hardware/lineage/compat
-        rm -rf hardware/mediatek
-        rm -rf hardware/oplus
-        rm -rf device/oneplus/denniz-kernel
-        unset CLEAN_DT_REPOS
-    fi
+#!/bin/bash
+
+# Move to source root
+cd "$ANDROID_BUILD_TOP"
+
+# Clone dependencies
+echo Cloning repositories
+VT=vendor/oneplus/denniz
+if ! [ -a $VT ]; then git clone https://github.com/nattolecats/proprietary_vendor_oneplus_denniz vendor/oneplus/denniz
 fi
-echo start cloning repos
-VT=vendor/oneplus/denniz/denniz-vendor.mk
-if ! [ -a $VT ]; then git clone https://github.com/nishant6342/vendor_oneplus_denniz -b RMUI4-OSS vendor/oneplus/denniz
+FW=vendor/oneplus/denniz-firmware
+if ! [ -d $FW ]; then git clone https://github.com/nattolecats/android_vendor_oneplus_denniz-firmware vendor/oneplus/denniz-firmware
 fi
-KT=kernel/oneplus/denniz/Makefile
-if ! [ -a $KT ]; then git clone --depth=1 https://github.com/nishant6342/kernel_oneplus_denniz -b T kernel/oneplus/denniz
+KERNEL=device/oneplus/denniz-kernel
+if ! [ -d $KERNEL ]; then git clone https://github.com/nattolecats/android_device_oneplus_denniz-kernel device/oneplus/denniz-kernel
 fi
-PA=packages/apps/prebuilt-apps/prebuilt-apps.mk
-if ! [ -a $PA ]; then git clone --depth=1 https://gitlab.com/nishant6342/packages_apps_prebuilt-apps packages/apps/prebuilt-apps/
+MTK_SEPOLICY=device/mediatek/sepolicy_vndr
+if ! [ -d $MTK_SEPOLICY ]; then git clone https://github.com/Project-Elixir/device_mediatek_sepolicy_vndr -b UNO device/mediatek/sepolicy_vndr
 fi
-MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
-if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/Project-Elixir/device_mediatek_sepolicy_vndr -b UNO device/mediatek/sepolicy_vndr
+MTK=hardware/mediatek
+if ! [ -d $MTK ]; then git clone https://github.com/nishant6342/android_hardware_mediatek -b lineage-20 hardware/mediatek
 fi
-PARTS=packages/apps/RealmeParts/parts.mk
-if ! [ -a $PARTS ]; then git clone https://github.com/nishant6342/packages_apps_RealmeParts packages/apps/RealmeParts
+WLAN=hardware/mediatek/wlan
+if ! [ -d $WLAN ]; then git clone https://github.com/nishant6342/android_hardware_mediatek_wlan hardware/mediatek/wlan
 fi
-POCKET=packages/apps/PocketMode/pocket_mode.mk
-if ! [ -a $POCKET ]; then git clone https://github.com/nishant6342/packages_apps_PocketMode packages/apps/PocketMode
+OPLUS=hardware/oplus
+if ! [ -d $OPLUS ]; then git clone https://github.com/nishant6342/android_hardware_oplus hardware/oplus
 fi
-FW=vendor/oneplus/denniz-firmware/Android.mk
-if ! [ -a $FW ]; then git clone https://github.com/nattolecats/android_vendor_oneplus_denniz-firmware vendor/oneplus/denniz-firmware
+COMPACT=hardware/lineage
+if ! [ -d $COMPACT ]; then git clone https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-20.0 hardware/lineage/compat
 fi
-CAM=device/oplus/camera/camera.mk
-if ! [ -a $CAM ]; then git clone --depth=1 https://gitlab.com/nishant6342/device_oplus_camera device/oplus/camera
+CLANG17=prebuilts/clang/host/linux-x86/clang-r487747
+if ! [ -d $CLANG17 ]; then git clone https://gitlab.com/projectelixiros/android_prebuilts_clang_host_linux-x86_clang-r487747 prebuilts/clang/host/linux-x86/clang-r487747
 fi
-COMPACT=hardware/lineage/compat/Android.bp
-if ! [ -a $COMPACT ]; then git clone https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-20.0 hardware/lineage/compat
+PARTS=packages/apps/RealmeParts
+if ! [ -d $PARTS ]; then git clone https://github.com/nishant6342/packages_apps_RealmeParts packages/apps/RealmeParts
 fi
-MTK=hardware/mediatek/Android.bp
-if ! [ -a $MTK ]; then git clone https://github.com/nishant6342/android_hardware_mediatek -b lineage-20 hardware/mediatek
+POCKET=packages/apps/PocketMode
+if ! [ -d $POCKET ]; then git clone https://github.com/nishant6342/packages_apps_PocketMode packages/apps/PocketMode
 fi
-CLANG17=prebuilts/clang/host/linux-x86/clang-r487747/bin/clang
-if ! [ -a $CLANG17 ]; then git clone https://gitlab.com/projectelixiros/android_prebuilts_clang_host_linux-x86_clang-r487747 prebuilts/clang/host/linux-x86/clang-r487747
-fi
-OPLUS=hardware/oplus/Android.mk
-if ! [ -a $OPLUS ]; then git clone https://github.com/nishant6342/android_hardware_oplus hardware/oplus
-fi
-WLAN=hardware/mediatek/wlan/Android.mk
-if ! [ -a $WLAN ]; then git clone https://github.com/nishant6342/android_hardware_mediatek_wlan hardware/mediatek/wlan
-fi
-KERNEL_PREBUILT=device/oneplus/denniz-kernel/dtbo.img
-if ! [ -a $KERNEL_PREBUILT ]; then git clone https://github.com/nattolecats/android_device_oneplus_denniz-kernel device/oneplus/denniz-kernel
-fi
-echo end cloning
+echo Done
